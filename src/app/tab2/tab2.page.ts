fead84391be90d7bc58e8eaf1ec7ac44c8f25d40
tensa132 <c14180033@john.petra.ac.id>
2021-03-16 21:04:17 +0700

    Soal Nomor 3

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,3 +1,4 @@
+import { stringify } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
 
 @Component({
@@ -9,4 +10,26 @@ export class Tab2Page {
 
   constructor() {}
 
+  cek : Boolean;
+  kata : String = '';
+  inputTebak : string = '';
+  randomNum : number = Math.floor(Math.random() * 5);
+
+  ngOnInit() {
+    console.log(this.randomNum);
+  }
+
+  tebak() {
+    // console.log(this.randomNum);
+    if(this.inputTebak == this.randomNum.toString()) {
+      this.cek = true
+      this.kata = 'Tebakan anda benar'
+      this.randomNum = Math.floor(Math.random() * 5)
+    } else {
+      this.randomNum = Math.floor(Math.random() * 5)
+      this.cek = false
+      this.kata = 'Harap coba kembali'
+      console.log(this.randomNum);
+    }
+  }
 }
